18f72c24c37624e3b403387afddfd432d18df07d
Bram SIerhuis <35657766+BramSierhuis@users.noreply.github.com>
2020-10-07 17:58:19 +0200

    Started working on resolving tickets

---
+++ View/Resolve Ticket.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace View
+{
+    partial class Resolve_Ticket
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ResolveTicket_lblTicketID = new System.Windows.Forms.Label();
+            this.ResolveTicket_lblName = new System.Windows.Forms.Label();
+            this.ResolveTicket_lblMail = new System.Windows.Forms.Label();
+            this.ResolveTicket_lblSubject = new System.Windows.Forms.Label();
+            this.ResolveTicket_btnResolve = new System.Windows.Forms.Button();
+            this.ResolveTicket_btnCancel = new System.Windows.Forms.Button();
+            this.ResolveTicket_lblDescription = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.ResolveTicket_lblStatus = new System.Windows.Forms.Label();
+            this.ResolveTicket_lblCategory = new System.Windows.Forms.Label();
+            this.ResolveTicket_lblPriority = new System.Windows.Forms.Label();
+            this.ResolveTicket_lblDeadline = new System.Windows.Forms.Label();
+            this.ResolveTicket_lblDate = new System.Windows.Forms.Label();
+            this.ResolveTicket_txtAreaSolution = new System.Windows.Forms.TextBox();
+            this.label18 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // ResolveTicket_lblTicketID
+            // 
+            this.ResolveTicket_lblTicketID.AutoSize = true;
+            this.ResolveTicket_lblTicketID.Location = new System.Drawing.Point(13, 13);
+            this.ResolveTicket_lblTicketID.Name = "ResolveTicket_lblTicketID";
+            this.ResolveTicket_lblTicketID.Size = new System.Drawing.Size(97, 16);
+            this.ResolveTicket_lblTicketID.TabIndex = 0;
+            this.ResolveTicket_lblTicketID.Text = "Ticket #xxxxxxx";
+            // 
+            // ResolveTicket_lblName
+            // 
+            this.ResolveTicket_lblName.AutoSize = true;
+            this.ResolveTicket_lblName.Location = new System.Drawing.Point(16, 43);
+            this.ResolveTicket_lblName.Name = "ResolveTicket_lblName";
+            this.ResolveTicket_lblName.Size = new System.Drawing.Size(91, 16);
+            this.ResolveTicket_lblName.TabIndex = 1;
+            this.ResolveTicket_lblName.Text = "Bram Sierhuis";
+            // 
+            // ResolveTicket_lblMail
+            // 
+            this.ResolveTicket_lblMail.AutoSize = true;
+            this.ResolveTicket_lblMail.Location = new System.Drawing.Point(19, 59);
+            this.ResolveTicket_lblMail.Name = "ResolveTicket_lblMail";
+            this.ResolveTicket_lblMail.Size = new System.Drawing.Size(143, 16);
+            this.ResolveTicket_lblMail.TabIndex = 4;
+            this.ResolveTicket_lblMail.Text = "bram@bramsierhuis.nl";
+            // 
+            // ResolveTicket_lblSubject
+            // 
+            this.ResolveTicket_lblSubject.AutoSize = true;
+            this.ResolveTicket_lblSubject.Location = new System.Drawing.Point(282, 13);
+            this.ResolveTicket_lblSubject.Name = "ResolveTicket_lblSubject";
+            this.ResolveTicket_lblSubject.Size = new System.Drawing.Size(95, 16);
+            this.ResolveTicket_lblSubject.TabIndex = 6;
+            this.ResolveTicket_lblSubject.Text = "Kapotte laptop";
+            // 
+            // ResolveTicket_btnResolve
+            // 
+            this.ResolveTicket_btnResolve.Location = new System.Drawing.Point(643, 415);
+            this.ResolveTicket_btnResolve.Name = "ResolveTicket_btnResolve";
+            this.ResolveTicket_btnResolve.Size = new System.Drawing.Size(75, 23);
+            this.ResolveTicket_btnResolve.TabIndex = 7;
+            this.ResolveTicket_btnResolve.Text = "Resolve";
+            this.ResolveTicket_btnResolve.UseVisualStyleBackColor = true;
+            this.ResolveTicket_btnResolve.Click += new System.EventHandler(this.ResolveTicket_btnResolve_Click);
+            // 
+            // ResolveTicket_btnCancel
+            // 
+            this.ResolveTicket_btnCancel.Location = new System.Drawing.Point(562, 415);
+            this.ResolveTicket_btnCancel.Name = "ResolveTicket_btnCancel";
+            this.ResolveTicket_btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.ResolveTicket_btnCancel.TabIndex = 8;
+            this.ResolveTicket_btnCancel.Text = "Cancel";
+            this.ResolveTicket_btnCancel.UseVisualStyleBackColor = true;
+            this.ResolveTicket_btnCancel.Click += new System.EventHandler(this.ResolveTicket_btnCancel_Click);
+            // 
+            // ResolveTicket_lblDescription
+            // 
+            this.ResolveTicket_lblDescription.AutoSize = true;
+            this.ResolveTicket_lblDescription.Location = new System.Drawing.Point(285, 43);
+            this.ResolveTicket_lblDescription.Name = "ResolveTicket_lblDescription";
+            this.ResolveTicket_lblDescription.Size = new System.Drawing.Size(426, 16);
+            this.ResolveTicket_lblDescription.TabIndex = 9;
+            this.ResolveTicket_lblDescription.Text = "Op een of andere manier heb ik mijn scherm gesloopt, het is gebarsten";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(22, 168);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(66, 16);
+            this.label9.TabIndex = 10;
+            this.label9.Text = "Category:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(22, 188);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(52, 16);
+            this.label10.TabIndex = 11;
+            this.label10.Text = "Priority:";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(25, 208);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(79, 16);
+            this.label11.TabIndex = 12;
+            this.label11.Text = "Deadline in:";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(22, 228);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(107, 16);
+            this.label12.TabIndex = 13;
+            this.label12.Text = "Date of issueing:";
+            // 
+            // ResolveTicket_lblStatus
+            // 
+            this.ResolveTicket_lblStatus.AutoSize = true;
+            this.ResolveTicket_lblStatus.Location = new System.Drawing.Point(610, 13);
+            this.ResolveTicket_lblStatus.Name = "ResolveTicket_lblStatus";
+            this.ResolveTicket_lblStatus.Size = new System.Drawing.Size(101, 16);
+            this.ResolveTicket_lblStatus.TabIndex = 14;
+            this.ResolveTicket_lblStatus.Text = "UNRESOLVED";
+            // 
+            // ResolveTicket_lblCategory
+            // 
+            this.ResolveTicket_lblCategory.AutoSize = true;
+            this.ResolveTicket_lblCategory.Location = new System.Drawing.Point(169, 168);
+            this.ResolveTicket_lblCategory.Name = "ResolveTicket_lblCategory";
+            this.ResolveTicket_lblCategory.Size = new System.Drawing.Size(53, 16);
+            this.ResolveTicket_lblCategory.TabIndex = 15;
+            this.ResolveTicket_lblCategory.Text = "Printers";
+            // 
+            // ResolveTicket_lblPriority
+            // 
+            this.ResolveTicket_lblPriority.AutoSize = true;
+            this.ResolveTicket_lblPriority.Location = new System.Drawing.Point(172, 188);
+            this.ResolveTicket_lblPriority.Name = "ResolveTicket_lblPriority";
+            this.ResolveTicket_lblPriority.Size = new System.Drawing.Size(36, 16);
+            this.ResolveTicket_lblPriority.TabIndex = 16;
+            this.ResolveTicket_lblPriority.Text = "High";
+            // 
+            // ResolveTicket_lblDeadline
+            // 
+            this.ResolveTicket_lblDeadline.AutoSize = true;
+            this.ResolveTicket_lblDeadline.Location = new System.Drawing.Point(172, 208);
+            this.ResolveTicket_lblDeadline.Name = "ResolveTicket_lblDeadline";
+            this.ResolveTicket_lblDeadline.Size = new System.Drawing.Size(48, 16);
+            this.ResolveTicket_lblDeadline.TabIndex = 17;
+            this.ResolveTicket_lblDeadline.Text = "3 days";
+            // 
+            // ResolveTicket_lblDate
+            // 
+            this.ResolveTicket_lblDate.AutoSize = true;
+            this.ResolveTicket_lblDate.Location = new System.Drawing.Point(172, 228);
+            this.ResolveTicket_lblDate.Name = "ResolveTicket_lblDate";
+            this.ResolveTicket_lblDate.Size = new System.Drawing.Size(65, 16);
+            this.ResolveTicket_lblDate.TabIndex = 18;
+            this.ResolveTicket_lblDate.Text = "10/7/2020";
+            // 
+            // ResolveTicket_txtAreaSolution
+            // 
+            this.ResolveTicket_txtAreaSolution.Location = new System.Drawing.Point(288, 168);
+            this.ResolveTicket_txtAreaSolution.Multiline = true;
+            this.ResolveTicket_txtAreaSolution.Name = "ResolveTicket_txtAreaSolution";
+            this.ResolveTicket_txtAreaSolution.Size = new System.Drawing.Size(423, 216);
+            this.ResolveTicket_txtAreaSolution.TabIndex = 19;
+            // 
+            // label18
+            // 
+            this.label18.AutoSize = true;
+            this.label18.Location = new System.Drawing.Point(288, 146);
+            this.label18.Name = "label18";
+            this.label18.Size = new System.Drawing.Size(59, 16);
+            this.label18.TabIndex = 20;
+            this.label18.Text = "Solution:";
+            // 
+            // Resolve_Ticket
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(734, 450);
+            this.Controls.Add(this.label18);
+            this.Controls.Add(this.ResolveTicket_txtAreaSolution);
+            this.Controls.Add(this.ResolveTicket_lblDate);
+            this.Controls.Add(this.ResolveTicket_lblDeadline);
+            this.Controls.Add(this.ResolveTicket_lblPriority);
+            this.Controls.Add(this.ResolveTicket_lblCategory);
+            this.Controls.Add(this.ResolveTicket_lblStatus);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.ResolveTicket_lblDescription);
+            this.Controls.Add(this.ResolveTicket_btnCancel);
+            this.Controls.Add(this.ResolveTicket_btnResolve);
+            this.Controls.Add(this.ResolveTicket_lblSubject);
+            this.Controls.Add(this.ResolveTicket_lblMail);
+            this.Controls.Add(this.ResolveTicket_lblName);
+            this.Controls.Add(this.ResolveTicket_lblTicketID);
+            this.Name = "Resolve_Ticket";
+            this.Text = "Resolve_Ticket";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label ResolveTicket_lblTicketID;
+        private System.Windows.Forms.Label ResolveTicket_lblName;
+        private System.Windows.Forms.Label ResolveTicket_lblMail;
+        private System.Windows.Forms.Label ResolveTicket_lblSubject;
+        private System.Windows.Forms.Button ResolveTicket_btnResolve;
+        private System.Windows.Forms.Button ResolveTicket_btnCancel;
+        private System.Windows.Forms.Label ResolveTicket_lblDescription;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label ResolveTicket_lblStatus;
+        private System.Windows.Forms.Label ResolveTicket_lblCategory;
+        private System.Windows.Forms.Label ResolveTicket_lblPriority;
+        private System.Windows.Forms.Label ResolveTicket_lblDeadline;
+        private System.Windows.Forms.Label ResolveTicket_lblDate;
+        private System.Windows.Forms.TextBox ResolveTicket_txtAreaSolution;
+        private System.Windows.Forms.Label label18;
+    }
+}
---
+++ View/Resolve Ticket.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Model;
+
+namespace View
+{
+    public partial class Resolve_Ticket : Form
+    {
+        public Resolve_Ticket(Ticket ticket)
+        {
+            InitializeComponent();
+
+            ResolveTicket_lblTicketID.Text = ticket.Id;
+            ResolveTicket_lblSubject.Text = ticket.Subject;
+            ResolveTicket_lblStatus.Text = ticket.Solved.ToString().ToUpper();
+
+            ResolveTicket_lblName.Text = ticket.Client.Name;
+            ResolveTicket_lblMail.Text = ticket.Client.Email;
+
+            ResolveTicket_lblCategory.Text = ticket.Category.ToString();
+            ResolveTicket_lblPriority.Text = ticket.Priority.ToString();
+            ResolveTicket_lblDeadline.Text = ticket.DaysToSolve.ToString();
+            ResolveTicket_lblDate.Text = ticket.DateOfIssueing.ToString("dd-MM-yyyy");
+        }
+
+        private void ResolveTicket_btnResolve_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ResolveTicket_btnCancel_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
