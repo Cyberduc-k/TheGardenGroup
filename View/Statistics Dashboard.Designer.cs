20a097ad79ca177e2e74b339078f58599e51c637
Tim850 <timkompier850@gmail.com>
2020-10-08 14:47:22 +0200

    Statistics Dashboard amount of tickets

--- View/Statistics Dashboard.Designer.cs
+++ View/Statistics Dashboard.Designer.cs
@@ -121,6 +121,7 @@
             this.Controls.Add(this.lbl_AmountOfActiveTickets);
             this.Name = "Tickets_Dashboard";
             this.Text = "Tickets_Dashboard";
+            this.Load += new System.EventHandler(this.Tickets_Dashboard_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- View/Statistics Dashboard.cs
+++ View/Statistics Dashboard.cs
@@ -7,14 +7,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Service;
+using Model;
 
 namespace View
 {
     public partial class Tickets_Dashboard : Form
     {
-        public Tickets_Dashboard()
+        private IServiceProvider provider;
+
+        public Tickets_Dashboard(IServiceProvider provider)
         {
+            this.provider = provider;
+
             InitializeComponent();
+        }
+
+        private void Tickets_Dashboard_Load(object sender, EventArgs e)
+        {
+            ITicketService ticketService = provider.GetService(typeof(ITicketService)) as ITicketService;
+
+            lbl_AmountOfActiveTickets.Text = ticketService.Count().ToString();
         }
     }
 }
