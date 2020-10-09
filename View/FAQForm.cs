af223c99cc034d5b1f96e02191d987460530972c
InvaderCipher <55230649+InvaderCipher@users.noreply.github.com>
2020-10-09 19:04:52 +0200

    you can now add FAQ and display them

--- View/FAQForm.cs
+++ View/FAQForm.cs
@@ -16,16 +16,18 @@ namespace View
     public partial class FAQForm : Form
     {
         private IServiceProvider provider;
+        private IFAQService faqService;
 
         public FAQForm(IServiceProvider provider)
         {
             this.provider = provider;
             InitializeComponent();
 
-            CategoryCombobox();
+            FillCategoryCombobox();
+            FillTreeView((Category)FAQ_comboCategory.SelectedItem);
         }
 
-        private void CategoryCombobox()
+        private void FillCategoryCombobox()
         {
             FAQ_comboCategory.Items.Clear();
 
@@ -40,44 +42,8 @@ namespace View
 
         private void FAQ_comboCategory_SelectedIndexChanged(object sender, EventArgs e)
         {
-
             treeView_FAQ.Nodes.Clear();
-
-            if ((Category)FAQ_comboCategory.SelectedItem == Category.General)
-            {
-                TreeNode generalQuestion1 = new TreeNode("General Question 1");
-                TreeNode answer1 = new TreeNode("Answer");
-                treeView_FAQ.Nodes.Add(generalQuestion1);
-                generalQuestion1.Nodes.Add(answer1);
-            }
-            else if ((Category)FAQ_comboCategory.SelectedItem == Category.Printers)
-            {
-                TreeNode printerQuestion1 = new TreeNode("Printer Question 1");
-                TreeNode answer1 = new TreeNode("Answer");
-                treeView_FAQ.Nodes.Add(printerQuestion1);
-                printerQuestion1.Nodes.Add(answer1);
-            }
-            else if ((Category)FAQ_comboCategory.SelectedItem == Category.Computers)
-            {
-                TreeNode computerQuestion1 = new TreeNode("Computer Question 1");
-                TreeNode answer1 = new TreeNode("Answer");
-                treeView_FAQ.Nodes.Add(computerQuestion1);
-                computerQuestion1.Nodes.Add(answer1);
-            }
-            else if ((Category)FAQ_comboCategory.SelectedItem == Category.Mail)
-            {
-                TreeNode mailQuestion1 = new TreeNode("Mail Question 1");
-                TreeNode answer1 = new TreeNode("Answer");
-                treeView_FAQ.Nodes.Add(mailQuestion1);
-                mailQuestion1.Nodes.Add(answer1);
-            }
-            else if ((Category)FAQ_comboCategory.SelectedItem == Category.Other)
-            {
-                TreeNode otherQuestion1 = new TreeNode("Other Question 1");
-                TreeNode answer1 = new TreeNode("Answer");
-                treeView_FAQ.Nodes.Add(otherQuestion1);
-                otherQuestion1.Nodes.Add(answer1);
-            }
+            FillTreeView((Category)FAQ_comboCategory.SelectedItem);
         }
 
         private void btn_AddQuestion_Click(object sender, EventArgs e)
@@ -89,7 +55,7 @@ namespace View
                 Category category = (Category)FAQ_comboCategory.SelectedItem;
 
                 FAQ newQuestion = new FAQ(question, answer, category);
-                IFAQService faqService = provider.GetService<IFAQService>();
+                faqService = provider.GetService<IFAQService>();
 
                 faqService.Add(newQuestion);
             }
@@ -97,7 +63,29 @@ namespace View
             {
                 ErrorHandler.Instance.HandleError("Something went wrong while adding your question, try again please.", "Try again", ex);
             }
+            finally
+            {
+                txt_AnswerAdd.Clear();
+                txt_QuestionAdd.Clear();
+            }
 
+        }
+
+        private void FillTreeView(Category category)
+        {
+            faqService = provider.GetService<IFAQService>();
+            List<FAQ> faqs = (List<FAQ>)faqService.GetAll();
+
+            foreach (FAQ f in faqs)
+            {
+                if (f.Category == category)
+                {
+                    TreeNode question = new TreeNode(f.Question);
+                    TreeNode answer = new TreeNode(f.Answer);
+                    treeView_FAQ.Nodes.Add(question);
+                    question.Nodes.Add(answer);
+                }
+            }
         }
     }
 }
