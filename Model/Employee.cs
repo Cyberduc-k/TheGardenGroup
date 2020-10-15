6075df69ff78c302b4f48ccc4285d6bf828d398b
Bram SIerhuis <35657766+BramSierhuis@users.noreply.github.com>
2020-10-15 15:13:52 +0200

    Renamed category to expertise

--- Model/Employee.cs
+++ Model/Employee.cs
@@ -4,16 +4,16 @@ namespace Model
 {
     public class Employee : User
     {
-        public Category Category { get; set; }
+        public Category Expertise { get; set; }
         public int ReviewScore { get; set; }
         public int TicketsProcessed { get; set; }
         public int TicketsSucceeded { get; set; }
         public int TicketsFailed { get; set; }
 
-        public Employee(string firstName, string lastName, DateTime birthDate, string email, string password, Category category)
+        public Employee(string firstName, string lastName, DateTime birthDate, string email, string password, Category expertise)
             : base(firstName, lastName, birthDate, email, password)
         {
-            Category = category;
+            Expertise = expertise;
             ReviewScore = 0;
             TicketsProcessed = 0;
             TicketsSucceeded = 0;
